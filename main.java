ad0f24c6e2f62eab593190edbf3763f5eb0da49a
Sandhiya021213 <145223855+Sandhiya021213@users.noreply.github.com>
2023-10-17 14:34:33 +0530

    Delete main.java

--- main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        BankAccount userAccount = new BankAccount(1000);
-        ATM atm = new ATM(userAccount);
-        atm.start();
-}
-}
